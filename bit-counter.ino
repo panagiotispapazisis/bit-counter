ffc0ff5de40092c9e4a4b767b8f03cc01f5a15fe
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-29 18:02:03 +0200

    final code for bit-counter project

--- bit-counter.ino
+++ bit-counter.ino
@@ -1,3 +1,11 @@
+int Number = 0;
+bool valueBtnInc;
+bool prevValueBtnInc = true;
+bool checkIncBtn;
+bool valueBtnDec;
+bool prevValueBtnDec = true;
+bool checkDecBtn;
+int count = 0;
 void setup()
 {
   for (int i = 4; i <= 11; i++)
@@ -13,11 +21,76 @@ void setup()
 void loop()
 {
 
-  while (Serial.available() == 0)
-    ;
-  int value = Serial.parseInt();
-  calculate(value);
+  valueBtnDec = digitalRead(2);
+  checkDecBtn = checkDecBtnState(valueBtnDec, prevValueBtnDec);
+  if (checkDecBtn)
+  {
+
+    count++;
+    if (count % 2 == 0)
+    {
+
+      if (Number > 0)
+      {
+        Number = Number - 1;
+        calculate(Number);
+      }
+
+      count = 0;
+    }
+  }
+
+  valueBtnInc = digitalRead(3);
+  checkIncBtn = checkIncBtnState(valueBtnInc, prevValueBtnInc);
+  if (checkIncBtn)
+  {
+
+    count++;
+    if (count % 2 == 0)
+    {
+
+      if (Number < 255)
+      {
+        Number = Number + 1;
+        calculate(Number);
+      }
+
+      count = 0;
+    }
+  }
+
+  prevValueBtnDec = valueBtnDec;
+  prevValueBtnInc = valueBtnInc;
 }
+
+bool checkIncBtnState(bool now, bool before)
+{
+
+  if (now != before)
+  {
+
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool checkDecBtnState(bool now, bool before)
+{
+
+  if (now != before)
+  {
+
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 void calculate(int val)
 {
 
