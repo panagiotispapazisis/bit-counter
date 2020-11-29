7020189dd3727c7558250779b2d289a75b4e7c95
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-29 15:03:24 +0200

    check if leds work

--- bit-counter.ino
+++ bit-counter.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
 
+void setup()
+{
+
+  for (int i = 4; i <= 11; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
+  for (int i = 4; i <= 11; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+
+  for (int i = 11; i >= 4; i--)
+  {
+    digitalWrite(i, LOW);
+    delay(100);
+  }
 }
