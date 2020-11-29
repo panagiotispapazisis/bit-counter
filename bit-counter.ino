ee585e1bee5bd9b8168f2bcc361ada1a75783452
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-29 15:23:22 +0200

    check if leds and buttons work

--- bit-counter.ino
+++ bit-counter.ino
@@ -1,9 +1,30 @@
 void setup()
 {
-  // put your setup code here, to run once:
+  for (int i = 4; i <= 11; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+
+  for (int i = 4; i <= 11; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+
+  for (int i = 11; i >= 4; i--)
+  {
+
+    digitalWrite(i, LOW);
+    delay(100);
+  }
+  Serial.println(digitalRead(2));
+  Serial.println(digitalRead(3));
 }
