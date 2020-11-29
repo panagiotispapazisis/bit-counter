24168b422b57ea5b3fc3d4bc71ec582e1e44cf44
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-29 15:06:32 +0200

    create bit-counter

--- bit-counter.ino
+++ bit-counter.ino
@@ -1,25 +1,9 @@
-
 void setup()
 {
-
-  for (int i = 4; i <= 11; i++)
-  {
-    pinMode(i, OUTPUT);
-  }
+  // put your setup code here, to run once:
 }
 
 void loop()
 {
-
-  for (int i = 4; i <= 11; i++)
-  {
-    digitalWrite(i, HIGH);
-    delay(100);
-  }
-
-  for (int i = 11; i >= 4; i--)
-  {
-    digitalWrite(i, LOW);
-    delay(100);
-  }
+  // put your main code here, to run repeatedly:
 }
